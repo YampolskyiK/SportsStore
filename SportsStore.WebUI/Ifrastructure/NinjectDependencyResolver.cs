2435b3794d46939a6898f389eb28b6b1798f12d4
Kostia <yampolskyi.kostiantyn@gmail.com>
2017-03-05 23:18:56 +0200

    first commit

---
+++ SportsStore.WebUI/Ifrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Ifrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+
+        private void AddBindings()
+        {
+;
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);        
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
